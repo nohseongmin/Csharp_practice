cb906ea4ef9bb2e9e96dd4f76d18ce1cb484574c
노성민 <85987504+nohseongmin@users.noreply.github.com>
2025-03-12 14:37:58 +0900

    Create FormMain.cs

---
+++ week01/FormMain.cs
@@ -0,0 +1,32 @@
+//GUI 부분(코드)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week02Proj01
+{
+    public partial class FormMain: Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
